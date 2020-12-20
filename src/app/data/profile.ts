61581994a2b711857a8990c97860d3290d2282bd
Allan Galarza <allan.galarza@gmail.com>
2020-12-19 19:12:32 -0700

    Added base work experience accordion

--- src/app/data/profile.ts
+++ src/app/data/profile.ts
@@ -1,12 +1,32 @@
 export let profile = {
     headline: 'Hello, my name is Allan!',
     aboutMe: 'I am a mechatronics engineer and software developer. I\'m mainly a backend developer, my favorite is Python. I love electronics, gaming and learning new things.',
-    workExperience: [
+    languages: [
         {
-            company: 'Encora'
+            name: 'Spanish',
+            level: 'Native'
         },
         {
-            company: 'Gila'
+            name: 'English',
+            level: 'Professional'
+        },
+        {
+            name: 'Portuguese',
+            level: 'Basic'
+        }
+    ],
+    workExperience: [
+        {
+            company: 'Encora',
+            start: '2020-11-09'
+        },
+        {
+            company: 'Gila Software',
+            start: '2019-05-13',
+            end: '2020-10-27'
+        },
+        {
+            company: 'LyF Ingeniería'
         }
     ]
 };
