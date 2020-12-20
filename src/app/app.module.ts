afbe903cef24560439990e87b15cfb260184f104
Allan Galarza <allan.galarza@gmail.com>
2020-12-19 18:21:05 -0700

    Added basic layout structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
-
-
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FlexLayoutModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -24,6 +26,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
     MatToolbarModule,
     MatCardModule,
     MatGridListModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data/profile.ts
@@ -0,0 +1,12 @@
+export let profile = {
+    headline: 'Hello, my name is Allan!',
+    aboutMe: 'I am a mechatronics engineer and software developer. I\'m mainly a backend developer, my favorite is Python. I love electronics, gaming and learning new things.',
+    workExperience: [
+        {
+            company: 'Encora'
+        },
+        {
+            company: 'Gila'
+        }
+    ]
+};
