c273a5ce0ed2976b4c69bea96841bedc0366e9f8
Allan Galarza <allan.galarza@gmail.com>
2020-12-19 21:52:50 -0700

    Added margins between components

--- src/app/data/profile.ts
+++ src/app/data/profile.ts
@@ -18,15 +18,37 @@ export let profile = {
     workExperience: [
         {
             company: 'Encora',
-            start: '2020-11-09'
+            start: new Date('2020-11-09')
         },
         {
             company: 'Gila Software',
-            start: '2019-05-13',
-            end: '2020-10-27'
+            start: new Date('2019-05-13'),
+            end: new Date('2020-10-27')
         },
         {
-            company: 'LyF Ingeniería'
+            company: 'LyF Ingeniería',
+            start: new Date('2018-01-16'),
+            end: new Date('2019-05-17')
+        },
+        {
+            company: 'I3',
+            start: new Date('2015-06-12'),
+            end: new Date('2017-12-31')
+        },
+        {
+            company: 'Universidad de Sonora',
+            start: new Date('2015-03-01'),
+            end: new Date('2020-05-01')
+        },
+        {
+            company: 'Grupo TEPEI',
+            start: new Date('2014-06-01'),
+            end: new Date('2014-11-01')
+        },
+        {
+            company: 'Plataforma Solar de Hermosillo',
+            start: new Date('2014-01-01'),
+            end: new Date('2014-06-01')
         }
     ]
 };
